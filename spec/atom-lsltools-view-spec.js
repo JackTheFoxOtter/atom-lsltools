a599c7534b90ba8779e278f45253345e20c038df
JackTheFoxOtter <jackthefoxotter@gmail.com>
2019-07-24 17:28:45 +0200

    initial commit

---
+++ spec/atom-lsltools-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomLsltoolsView from '../lib/atom-lsltools-view';
+
+describe('AtomLsltoolsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
