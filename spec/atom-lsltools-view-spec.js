c3ffe4c97ba9b21d1f8d78277cbff97018531650
JackTheFoxOtter <jackthefoxotter@gmail.com>
2019-07-26 22:22:08 +0200

    + Added console view and functionality * Updated style * Updated keymap - Removed fragments of default plugin

--- spec/atom-lsltools-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomLsltoolsView from '../lib/atom-lsltools-view';
-
-describe('AtomLsltoolsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
